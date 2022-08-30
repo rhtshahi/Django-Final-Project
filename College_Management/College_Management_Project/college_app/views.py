19335cd2a4fb9961825d976607b720ba6be131c5
unknown <iamshahirohit@gmail.com>
2022-08-30 18:26:24 +0545

    Authentication

--- College_Management/College_Management_Project/college_app/views.py
+++ College_Management/College_Management_Project/college_app/views.py
@@ -2,8 +2,13 @@ from multiprocessing import context
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+#from College_Management_Project.college_app.decorators import unauthenticated_user
+from .forms import CreateUserForm
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.decorators import login_required
+
+from .decorators import unauthenticated_user
 
 # Create your views here.
 
@@ -14,8 +19,68 @@ def homePage(request):
 def about(request):
     return render(request, 'about.html')
 
+
 def loginPage(request):
+
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            #username = request.user.username latest: request.user.is_authenticated
+            return redirect('student-home')
+
+        else:
+            messages.info(request, 'Incorrect Username/Password!!!')
+            return render(request, 'login.html')
+
     return render(request, 'login.html')
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@unauthenticated_user
 def registerPage(request):
-    return render(request, 'register.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user_name )
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+        # form = CreateUserForm()
+
+        # if request.method == 'POST':
+        #     form = CreateUserForm(request.POST)
+        #     if form.is_valid():
+        #         form.save()
+        #         user_name = form.cleaned_data.get('username')
+        #         messages.success(request, 'Account was created for ' + user_name )
+        #         return redirect('login')
+
+        # context = {'form':form}
+        # return render(request, 'register.html', context)
+
+@login_required(login_url='login')
+def adminPage(request):
+    return render(request, 'adminHome.html')
+
+
+@login_required(login_url='login')
+def teacherPage(request):
+    return render(request, 'teacherHome.html')
+
+
+@login_required(login_url='login')
+def studentPage(request):
+    return render(request, 'studentHome.html')
